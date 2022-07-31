18668a5099485039a39b42e714179b51ca4ccb55
yuvalgd23-meet <109908289+yuvalgd23-meet@users.noreply.github.com>
2022-07-31 23:37:45 +0300

    done but still working to make it nicer

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,10 +4,26 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods=['POST', 'GET']) # What methods are needed?
 def home():
+
+		if request.method == 'POST':
+			try:
+				in1 = request.form['fq']
+				login_session['fq'] = request.form['fq']
+				in2 = request.form['q']
+				login_session['q'] = request.form['q']
+				in3 = request.form['aa']
+				login_session['aa'] = request.form['aa']
+				if login_session['fq'] == '' or login_session['q'] == '' or login_session['aa'] == '':
+					return redirect(url_for("error"))
+				else:
+					return render_template('thanks.html')
+			except: 
+				return render_template('error.html')
 	
-	return render_template('home.html')
+		else:
+			return render_template('home.html')
 
 
 @app.route('/error')
@@ -19,13 +35,13 @@ def error():
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', login_session = login_session) # What variables are needed?
 
 
 @app.route('/thanks')
 def thanks():
 
-	return render_template('thanks.html')
+	return render_template('thanks.html', login_session = login_session)
 
 
 if __name__ == '__main__':
